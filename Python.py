d9de81830095453d5b44d59ef376cc685e3b92d7
alepou465714 <alepou465714@gmail.com>
2025-11-15 12:05:08 +0200

    python turtle???

--- Python.py
+++ Python.py
@@ -1,5 +1,19 @@
 password = input("Enter password: ")
 if password == "1234":
     print("Welcome")
+    import turtle
+    turtle.pencolor("brown")
+    turtle.pensize(20)
+    turtle.bgcolor("white")
+    for i in range(1):
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.back(100)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.back(100)
 else:
     print("Try again")
